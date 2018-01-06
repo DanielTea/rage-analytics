6f7a23756b610ae6607b156eb78ebf867fc40230
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2018-01-06 15:45:23 +0100

    testing non-cnn version

--- engine/realtime_FaceTrimmer.py
+++ engine/realtime_FaceTrimmer.py
@@ -6,23 +6,29 @@
 from PIL import Image
 import face_recognition
 
-# Load the jpg file into a numpy array
-image = face_recognition.load_image_file("biden.jpg")
 
-# Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
+def face_recognition_tester(image_path):
+    # Load the jpg file into a numpy array
+    image = face_recognition.load_image_file(image_path)
 
-print("I found {} face(s) in this photograph.".format(len(face_locations)))
+    # Find all the faces in the image using the default HOG-based model.
+    # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+    # See also: find_faces_in_picture_cnn.py
+    face_locations = face_recognition.face_locations(image)
 
-for face_location in face_locations:
+    print("I found {} face(s) in this photograph.".format(len(face_locations)))
 
-    # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+    for face_location in face_locations:
 
-    # You can access the actual face itself like this:
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    pil_image.show()
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+        # You can access the actual face itself like this:
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        pil_image.show()
+
+#face_recognition_tester("images/streamer_1.png")
+#face_recognition_tester("images/streamer_2.png")
+face_recognition_tester("images/streamer_3.png")
