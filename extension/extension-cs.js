1a97f6c27215cf50320a96233739f9541ae5b9f3
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2018-01-18 17:50:01 +0100

    fixed streamer name

--- extension/extension-cs.js
+++ extension/extension-cs.js
@@ -279,7 +279,7 @@ function createCustomNotification(streamer) {
                height="18px" 
                version="1.1" 
                viewBox="0 0 16 16"
-               onclick=removeCustomNotification(` + streamer.name +`)>
+               onclick=removeCustomNotification("` + streamer.name +`")>
             <path d="M8 6.586L3.757 2.343 2.343 3.757 6.586 8l-4.243 4.243 1.414 1.414L8 9.414l4.243 4.243 1.414-1.414L9.414 8l4.243-4.243-1.414-1.414" 
                   fill-rule="evenodd">       
             </path>
@@ -320,8 +320,10 @@ function createCustomNotification(streamer) {
 }
 
 function removeCustomNotification(streamerName) {
-   document.getElementById("notification_" + streamerName).remove();
-  alert("removed element " + streamerName )
+
+    console.log("getting element " + "notification_" + streamerName)
+    document.getElementById("notification_" + streamerName).remove();
+    alert("removed element " + streamerName )
 }
 
 
